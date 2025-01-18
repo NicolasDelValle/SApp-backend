4f508232fbca4b8f853b87dff1da9acdcebd96dc
Nicolas Del Valle <50749255+NicolasDelValle@users.noreply.github.com>
2025-01-17 23:09:26 -0300

    prew commit fix

--- migrations/20250111200932-update-users-table.js
+++ migrations/20250111200932-update-users-table.js
@@ -15,13 +15,11 @@ module.exports = {
       unique: true,
     });
 
-    // Agregar la columna "picture" si no existe
     await queryInterface.addColumn("Users", "picture", {
       type: Sequelize.STRING,
       allowNull: true,
     });
 
-    // Agregar la columna "email" si no existe
     await queryInterface.addColumn("Users", "email", {
       type: Sequelize.STRING,
       allowNull: false,
@@ -30,16 +28,13 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    // Revertir la columna "auth0Id" al tipo INTEGER
     await queryInterface.changeColumn("Users", "auth0Id", {
       type: Sequelize.INTEGER,
       allowNull: true,
     });
 
-    // Remover la columna "picture"
     await queryInterface.removeColumn("Users", "picture");
 
-    // Remover la columna "email"
     await queryInterface.removeColumn("Users", "email");
   },
 };
